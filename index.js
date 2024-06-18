42efcde186a3925c50a7aba309fc7a6ac27fea34
Devananda Binu <deva31807@gmail.com>
2024-06-18 22:22:42 +0100

    Added a clear function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const container = document.querySelector(".container");
 const display = document.querySelector("#display");
+const deleteBtn = document.querySelector("#delete");
 
 function appendToDisplay(input) {
     display.value += input;
 }
+
+function clear() {
+    display.value = "";
+}
+
+deleteBtn.addEventListener("click", clear);
