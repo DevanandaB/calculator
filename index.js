f0349514c2abcdc1a5245b6d6fb6aab5560d5e2e
Devananda Binu <deva31807@gmail.com>
2024-06-18 21:55:05 +0100

    Added a style in css and some functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const container = document.querySelector(".container");
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+function add() {};
+
+function subtract() {};
+
+function multiply() {};
+
+function division() {};
