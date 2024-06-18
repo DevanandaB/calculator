ce2eafbb2b607432bfaa947179b6a5e743acbfec
Devananda Binu <deva31807@gmail.com>
2024-06-18 22:35:20 +0100

    Added some functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".container");
 const display = document.querySelector("#display");
 const deleteBtn = document.querySelector("#delete");
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
 
 function appendToDisplay(input) {
     display.value += input;
@@ -8,6 +10,14 @@ function appendToDisplay(input) {
 
 function clear() {
     display.value = "";
+}
+
+function sum(a, b) {
+    return a + b; 
+}
+
+function difference(a, b) {
+    return a + b; 
 }
 
 deleteBtn.addEventListener("click", clear);
